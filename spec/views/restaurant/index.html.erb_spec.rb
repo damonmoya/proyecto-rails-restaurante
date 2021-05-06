5664a11a7e35f0c919d832edd45c2b147aed9267
Damon Garcia Garcia <damon99@hotmail.es>
2021-05-06 21:44:37 +0100

    Cambios en tests

--- spec/views/restaurant/index.html.erb_spec.rb
+++ spec/views/restaurant/index.html.erb_spec.rb
@@ -6,6 +6,16 @@ RSpec.describe "restaurant/index.html.erb", type: :view do
   end
   it 'displays index page' do
     render
-    expect(rendered).to have_content 'Mi restaurante'
+    expect(rendered).to have_content 'Nueva reserva'
+    assert_select "form[action=?][method=?]", restaurant_index_path, "post" do
+      assert_select "input[name=?]", "email"
+      assert_select "select", 5
+      assert_select "input[name=?]", "diners"
+    end
+    expect(rendered).to have_content 'Consultar reservas'
+    assert_select "form[action=?][method=?]", restaurant_mybooks_path, "post" do
+      assert_select "input[name=?]", "email"
+    end
+
   end
 end
