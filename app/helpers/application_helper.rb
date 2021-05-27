2c885704886168810e29a1258a82d87a89f92378
Damon Garcia Garcia <damon99@hotmail.es>
2021-05-27 09:50:38 +0100

    Revisado helper sortable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,13 +3,6 @@ module ApplicationHelper
         sort_column = params[:sort] || "email"
         sort_direction = params[:direction] || "asc"
         direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-        link_to title, {:sort => column, :direction => direction}
-    end
-
-    def sortable_mybooks(column, title)
-        sort_column = params[:sort] || "email"
-        sort_direction = params[:direction] || "asc"
-        direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
         link_to title, {:sort => column, :direction => direction, :token => params[:token]}
     end
 end
